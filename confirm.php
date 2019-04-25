7ff1eef958f73c2e611ff63ba1829fde43cb7984
yuma0000 <38005160+yuma0000@users.noreply.github.com>
2019-04-25 21:42:26 +0900

    Create confirm.php

---
+++ confirm.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+  </head>
+  <body>
+    <?php
+      mb_language("Japanese");
+      mb_internal_encoding("UTF-8");
+      $to = $_POST['to'];
+      $title = $_POST['title'];
+      $content = $_POST['content'];
+      if(mb_send_mail($to, $title, $content)){
+        echo "メールを送信しました";
+      } else {
+        echo "メールの送信に失敗しました";
+      };
+    ?>
+  </body>
+</html>
