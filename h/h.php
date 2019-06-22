3e4817b27de5d2d2fce596d4bd517eeab70544ac
yuma0000 <38005160+yuma0000@users.noreply.github.com>
2019-06-22 10:13:36 +0900

    Create h.php

---
+++ h/h.php
@@ -0,0 +1,33 @@
+<?php
+
+$name = $_POST['name'];
+$contents = $_POST['contents'];
+$contents = nl2br($contents);
+
+$data = "<hr>\r\n";
+$data = $data."<p>player:".$name."</p>\r\n";
+$data = $data."<p>text:</p>\r\n";
+$data = $data."<p>".$contents."</p>\r\n";
+
+ 
+$data = $data."";
+
+$keijban_file = 'keijiban.html';
+
+$fp = fopen($keijban_file, 'ab');
+
+if ($fp){
+    if (flock($fp, LOCK_EX)){
+        if (fwrite($fp,  $data) === FALSE){
+            print('ファイル書き込みに失敗しました');
+        }
+
+        flock($fp, LOCK_UN);
+    }else{
+        print('ファイルロックに失敗しました');
+    }
+}
+
+fclose($fp);
+
+?>
